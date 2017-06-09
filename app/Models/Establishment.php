480e14c6e92a3bf0d8885f09bd9e3d6ed22281ac
JP Caparas <jp@jpcaparas.com>
2017-06-10 11:42:27 +1200

    Add establishment model

---
+++ app/Models/Establishment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Establishment extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'address2',
+        'suburb',
+        'postcode',
+        'city',
+        'phone',
+    ];
+}
